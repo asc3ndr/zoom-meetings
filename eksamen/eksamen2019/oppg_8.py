4d4c7490877f7eed74199cf2c31ef22d6a28b3f5
Andreas Rovik <52315788+asc3ndr@users.noreply.github.com>
2020-11-15 14:45:05 +0100

    remove duplicate

--- eksamen/eksamen2019/oppg_8.py
+++ eksamen/eksamen2019/oppg_8.py
@@ -1,11 +1,5 @@
 def find_number(string):
     number = [int(symbol) for symbol in string if symbol.isnumeric()]
-
-    number = []
-    for symbol in string:
-        if symbol.isnumeric():
-            number.append(int(symbol))
-
     return sum(number)
 
 
